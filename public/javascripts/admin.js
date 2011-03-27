25f0a3e064bbb0d712c224574a3d6e2bb9d39871
Dane Pieri <dpieri@cmu-435163.wv.cc.cmu.edu>
2011-03-26 23:19:28 -0400

    more various

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
-	var leftbox_height=$('#leftbox').height();
-	var leftbox1_height=$('#leftbox1').height();
-	if(leftbox_height>leftbox1_height){
-		$('#leftbox1').height( $('#leftbox').height() );
-	}else{
-		$('#leftbox').height( $('#leftbox1').height() );
-	}
-	
+	fixHeights();
+	addListeners();
+});
+
+function addListeners(){
 	$('li a.folder').click(function(){
 		var loadUrl = $(this).attr('path');
 		$('ul#file_list').load(loadUrl, function(){
@@ -14,7 +11,7 @@ $(document).ready(function() {
 		});
 		
 	});
-});
+}
 function fixHeights(){
 	var leftbox_height=$('#leftbox').height();
 	var leftbox1_height=$('#leftbox1').height();
@@ -23,4 +20,6 @@ function fixHeights(){
 	}else{
 		$('#leftbox').height( $('#leftbox1').height() );
 	}
+	$("#file_list li:last").addClass("last");
+	addListeners();  //do this in case it is not an initial load
 }
