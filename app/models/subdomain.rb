75dfe88ccb822aee3eaf1e6255890262f605d5cc
Dane Pieri <dpieri@cmu-435163.wv.cc.cmu.edu>
2011-04-04 15:15:02 -0400

    chomd fix

--- app/models/subdomain.rb
+++ app/models/subdomain.rb
@@ -37,6 +37,7 @@ class Subdomain < ActiveRecord::Base
     Dir.foreach(directory) do |e|
       if File.directory?("#{directory}/#{e}")
         next if e == '.' || e == '..' || e == "__MACOSX"
+        File.chmod(0766, "#{directory}/#{e}")
         command = "mv #{directory}/#{e}/* #{directory}"
         system(command)
         #remove the zip parent directory this does not error gracefully
