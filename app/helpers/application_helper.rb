1bc377b0da0fd66dc5bcc31988e98cf0d354b0e5
Dane Pieri <dpieri@Macintosh-375.local>
2011-04-05 13:10:59 -0400

    asdf

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     date = Time.now.gmtime.strftime("%Y%m%d")
     key = 'f48f1f5fc910564a8f03a03bd455536c'
     hash = Digest::MD5.hexdigest("#{date}#{url}#{key}")
-    "http://webthumb.bluga.net/easythumb.php?url=#{url}&cache=1&hash=#{hash}&user=11144&size=large"
+    "http://webthumb.bluga.net/easythumb.php?url=#{url}&cache=-1&hash=#{hash}&user=11144&size=large"
   end
 end
