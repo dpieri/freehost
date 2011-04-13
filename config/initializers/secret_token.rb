d992db3b0e4b47fe31e954ca8f11617004f95fcc
Dane Pieri <dpieri@cmu-435163.wv.cc.cmu.edu>
2011-04-13 17:30:42 -0400

    session reset

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FreeHost::Application.config.secret_token = '83621368bcf34f899c5b3e3e25b23c8aef167db8faa11c3fb36249b0ea779e1b6a77a574546f3837417b67f590bb30ca7b3d5f9fd35ae430e3664c5e00b931b1'
+FreeHost::Application.config.secret_token = '00021368bcf34f899c5b3e3e25b23c8aef167db8faa11c3fb36249b0ea779e1b6a77a574546f3837417b67f590bb30ca7b3d5f9fd35ae430e3664c5e00b931b1'
